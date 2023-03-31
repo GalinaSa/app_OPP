ea9dceadf010c9c130d9b68702f31c2333b8981c
Galina Samotorova samotorovag@gmail.com <samotorovag@gmail.com>
2023-03-31 18:18:53 +0200

    Frontend final version

--- app.py
+++ app.py
@@ -1,108 +1,93 @@
 import streamlit as st
 import requests
-
+from PIL import Image
 import numpy as np
 import pandas as pd
-import joblib
+import time
+import altair as alt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-#st.set_page_config(
-            #page_title="Oil Production Prediction", # => Quick reference - Streamlit
-            #page_icon="🐍",
-            #layout="centered", # wide
-            #initial_sidebar_state="auto") # collapsed
+#st.set_page_config(layout="wide")
 
-# Style the page with CSS in we have some time ?
+df = pd.read_csv("prediction_forecast.csv")
+fig, ax = plt.subplots(figsize=(30,18))
+ax.plot(df['Date'], df['Real Oil rate'], **{'color': 'limegreen', 'linestyle': '','marker': 'o', 'markersize': 8, 'label':'Real oil production'})
+ax.plot(df['Date'], df['Predicted Oil rate'], **{'color': 'black', 'linewidth': 3, 'label':'Predicted oil production'})
+ax.xaxis.set_visible(False)
+ax.set_ylabel('Oil production [barrels]', fontsize=30)
+ax.legend(fontsize="30")
+plt.ylim(bottom=300)
 
-st.header("""Oil Production Prediction
-### Predict the **Oil Rate** based on different parameters:
-- Well head temperature and pressure
-- Amount of water and gas in production
-- Various pressures
----
-""")
+fig2, ax = plt.subplots(figsize=(30,18))
+ax.plot(df['Date'], df['Real Oil rate'], **{'color': 'limegreen', 'linestyle': '','marker': 'o', 'markersize': 8, 'label':'Real oil production'})
+ax.plot(df['Date'], df['Forecasted Oil'], **{'color': 'red', 'linestyle': '','marker': 'o', 'markersize': 4, 'label':'Forecasted Oil'})
+ax.plot(df['Date'], df['Predicted Oil rate'], **{'color': 'black', 'linewidth': 3, 'label':'Predicted oil production'})
+ax.xaxis.set_visible(False)
+ax.set_ylabel('Oil production [barrels]', fontsize=30)
+ax.legend(fontsize="30")
+plt.ylim(bottom=300)
 
-from PIL import Image
-image = Image.open('Image_OPP_big.png')
-st.image(image, caption='Oil, Water and Gas Production rates in one of the Wells', use_column_width=False)
-
-st.markdown("""
-#### *Please select the well*:
-""")
-st.checkbox("Well A")
-st.checkbox("Well B")
-
-#param1 = st.slider('Select a number', 1, 10, 3)
-#param2 = st.slider('Select another number', 1, 10, 3)
-#params = {
-#    'feature1': param1,
-#    'feature2': param2
-#}
+st.markdown("<h2 style='text-align: center; color: black;'>PREDICT OIL</h2>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: grey;'>We can predict the historical Oil Rates and also Forecast the future production! </h4>", unsafe_allow_html=True)
 
 
-baseline_model = joblib.load(open("baseline_model.pkl","rb"))
+image = Image.open('Image_hist4.jpg')
+st.image(image, use_column_width=True)
 
 st.markdown("""
-#### *Please select the parameters*:
+#### Please load your data:
 """)
-with st.form(key='params_for_api'):
+uploaded_file = st.file_uploader("Choose a CSV file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write("filename:", uploaded_file.name)
+    st.write(dataframe)
+if st.button('Make Prediction'):
+    time.sleep(10)
 
-    WHT = st.slider("Choose the Well Head Temperature", 0.0, 1.0, 0.05)
-    WHP = st.slider("Choose the Well Head Pressure", 0.0 , 0.6, 0.05)
-    Tubing_Gradient = st.slider("Choose the Tubing Gradient", 0.25 , 1.05, 0.05)
-    Service_Line_P = st.slider("Choose the Line Pressure", -0.05 , 0.65, 0.05)
-    Sand_Raw = st.slider("Choose the Sand Content", -0.2 , 0.8, 0.05)
-    Qgas_MPFM = st.slider("Choose the Gas Rate", 0.0 , 0.15, 0.01)
-    MPFM_WCT = st.slider("Choose the amount of Water in Production", 0.0 , 1.0, 0.05)
-    MPFM_Venturi_dP = st.slider("Choose the Pressure Difference", 0.0 , 0.4, 0.05)
-    MPFM_T = st.slider("Choose the Sensor Temperature", 0.0 , 1.0, 0.05)
-    Manifold_T = st.slider("Choose the Manifold Temperature", 0.1 , 1.0, 0.05)
-    Manifold_P = st.slider("Choose the Manifold Pressure", 0.2 , 1.1, 0.05)
-    Jumper_T = st.slider("Choose the Jumper Temperature", 0.1 , 1.0, 0.05)
-    Choke_Opening = st.slider("Choose the Choke Size", 0.1 , 0.9, 0.05)
-    Annulus_P = st.slider("Choose the Annulus Pressure", 0.2 , 0.95, 0.05)
+    #def get_line_chart_data():
 
-    st.form_submit_button('Make prediction')
+        #df = pd.read_csv("prediction_forecast.csv")
+        #return df
 
-dic = {
-    "Well Head Temperature": WHT,
-    "Well Head Pressure": WHP,
-    "Tubing Gradient": Tubing_Gradient,
-    "Service Line Pressure": Service_Line_P,
-    "Sand Content": Sand_Raw,
-    "Gas Rate": Qgas_MPFM,
-    "Amount of Water in Production": MPFM_WCT,
-    "Pressure Difference": MPFM_Venturi_dP,
-    "Sensor Temperature": MPFM_T,
-    "Manifold Temperature": Manifold_T,
-    "Manifold Pressure": Manifold_P,
-    "Jumper Temperature": Jumper_T,
-    "Choke size": Choke_Opening,
-    "Annulus Pressure": Annulus_P
-}
+    #df = get_line_chart_data()
+    #st.line_chart(df[['Real Oil rate', 'Predicted Oil rate']])
+
+    tab1, tab2, tab3 = st.tabs(["🗃 Impactful features","📈 Prediction Chart", "🗃 📈 Forecast Chart"])
+    tab1.subheader("List of the most impactful features")
+    tab1.markdown("""
+        - Pressure at the Well Head
+        - Amount of gas in produced oil
+        - Size of the choke
+        - Temperature
+    """)
+
+    tab2.subheader("Real vs. Predicted oil")
+    tab2.pyplot(fig)
+
+    tab3.subheader("Real vs. Forecast oil")
+    tab3.pyplot(fig2)
+
+
+    #time.sleep(10)
+    #with st.button('Done!'):
+            #st.balloons()
+
 
 #url = 'http://localhost:8080/predict'
 url = 'https://container-opp-6kchhmx67a-ew.a.run.app/predict'
-#response = requests.get(url, params=dic)
-#prediction = response.json()
-#st.text(response.json())
 
-df_test = pd.DataFrame.from_dict([dic])
-prediction = baseline_model.predict(df_test)
 
-#pred = prediction['Qoil MPFM']
+#My attemps with Altair Chart:
+#df['Date'] = df.index.values
+#line_chart1 = alt.Chart(df).mark_point(size=4, color='limegreen').encode(
+    #alt.X("Date", title="Date", axis=alt.Axis(labels=False)),
+    #alt.Y("Real Oil rate", title="Oil Production", scale=alt.Scale(domain=[1000,35000]),axis=alt.Axis(labels=False)),
+    #).properties(width=800, height=400)
 
-st.header(f'The estimated Oil Rate is: {int(prediction)} barrels')
-
-# We can also display the DF if needed:
-#st.dataframe(df_test)
-
-# Here will be the Oil Production plot (line chart) with the predicted values
-# and how they are compared to the real values
-@st.cache
-def get_line_chart_data():
-
-    df = pd.read_csv("prediction.csv")
-    return df
-
-df = get_line_chart_data()
-st.line_chart(df[['Qoil MPFM', 'predicted_Qoil']])
+#line_chart2 = alt.Chart(df).mark_line(size=1, color='black').encode(
+    #alt.X("Date"),
+    #alt.Y("Predicted Oil rate"),
+    #)
+#st.altair_chart(line_chart1 + line_chart2, use_container_width=True)
